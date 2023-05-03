17afe1fc193ec2adec13aa4a5c5ab8025eaa8342
Nathan <l.nathan97460@gmail.com>
2023-05-04 01:25:12 +0200

    Checkpoint Partie : Ajoutez structure et style à votre site grâce à un gabarit de base, du CSS et des fichiers statiques

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -2,24 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from listings.models import Band
+from listings.models import Listing
 
 def hello(request):
     bands = Band.objects.all()
-    return HttpResponse(f"""
-        <h1>Hello Django !</h1>
-        <p>Mes groupes préférés sont :<p>
-        <ul>
-            <li>{bands[0].name}</li>
-            <li>{bands[1].name}</li>
-            <li>{bands[2].name}</li>
-        </ul>
-    """)
+    return render(request,
+    'listings/hello.html',
+    {'bands': bands})
 
 def about(request):
-    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
+    return render(request,'listings/about.html')
 
 def listings(request):
-    return HttpResponse('<h1>listings</h1>')
+    listings = Listing.objects.all()
+    return render(request,
+    'listings/listings.html',
+    {'listings': listings})
 
 def contact(request):
-    return HttpResponse('<h1>contact</h1>')
+    return render(request,'listings/contact.html')
