3ad9ea5c8154d5a47613775523b8237aea1a6b67
Nathan LEBON <nathan.lebon@univ-toulouse.fr>
2023-05-04 17:33:42 +0200

    Checkpoint partie : Effectuez des opérations CRUD dans l'administration de Django

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from listings.models import Band
+
+class BandAdmin(admin.ModelAdmin):  # nous insérons ces deux lignes..
+    list_display = ('name', 'year_formed', 'genre') # liste les champs que nous voulons sur l'affichage de la liste
+
+admin.site.register(Band, BandAdmin)
---
+++ readme.md
@@ -0,0 +1,9 @@
+Créer un environnement virtuel `python -m venv env`
+
+Activer environnement virtuel `env\Scripts\activate`
+
+Installer les librairies **`pip install -r requirements.txt`**
+
+Lancer le serveur `python merchex/manage.py runserver`
+
+`python manage.py makemigrations` et `python manage.py migrate`
